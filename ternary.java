4c1b07b9a443f90fb409d60c3a88c77c82532278
jaswanthreddy519 <jaswanthreddymaramreddy@gmail.com>
2023-11-02 13:04:09 +0530

    hello

---
+++ ternary.java
@@ -0,0 +1,9 @@
+class Ternary
+{
+  public static void main(String args[])
+  {
+   int num1=10,num2=20,num3=30;
+  String number=(num1>num2>num3)?"grater":"smaller";
+  System.out.println(number);
+  }
+}
