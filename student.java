3961c724945364df44908370a40f17af8d0eb554
jaswanthreddy519 <jaswanthreddymaramreddy@gmail.com>
2023-11-03 11:56:49 +0530

    hello

---
+++ student.java
@@ -0,0 +1,45 @@
+class Student
+{
+  public static void main(String args[])
+  {
+   String student="jaswanth";
+   char id='M';
+   String branch="cse";
+   long number=8688665504l;
+   String mail="jaswanthreddy@gmail.com";
+   int yop=2023;
+   int m1=35;
+   int m2=45;
+   int m3=30;
+   int total=m1+m2+m3;
+   double avarage=m1+m2+m3/3;
+   double percentage=(total/300.0)*100;
+    if(total<100)
+       System.out.println(avarage+""+percentage);
+    else if(total==100)
+       System.out.println(avarage+""+percentage);
+    else if(total>100)
+       System.out.println(avarage+""+percentage);
+    else
+       System.out.println("fail");
+    
+       System.out.println(student);
+       System.out.println(id);
+       System.out.println(branch);
+       System.out.println(number);
+       System.out.println(mail);
+       System.out.println(yop);
+       System.out.println(m1);
+       System.out.println(m2);
+       System.out.println(m3);
+       System.out.println(total);
+       System.out.println(avarage);
+       System.out.println(percentage);
+       System.out.println("STUDENT APPLICATION");
+     }
+}
+
+
+
+
+
